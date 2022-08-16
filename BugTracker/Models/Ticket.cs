acbb22af2566ec8f76cc021b66564406b3d0a63d
Hyun Kong <konghyunsik7@gmail.com>
2022-08-15 21:19:07 -0400

    detailed the names

--- BugTracker/Models/Ticket.cs
+++ BugTracker/Models/Ticket.cs
@@ -4,11 +4,11 @@ namespace BugTracker.Models
     public class Ticket
     {
         public int TicketId { get; set; }
-        public string Type { get; set; }
+        public string ErrorID { get; set; }
         public string Description { get; set; }
-        public string Priorty { get; set; }
+        public string Priority { get; set; }
         public string Status { get; set; }
-        public string Owner { get; set; }
+        public string Submitter { get; set; }
 
  
     }
